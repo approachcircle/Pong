e7892a94f6528c1f9f81dffb568c52d85d0f1926
approachcircle <osc-mcd@protonmail.com>
2023-12-20 17:43:10 +0000

    set target framerate to 60

--- src/main/java/net/approachcircle/game/Main.java
+++ src/main/java/net/approachcircle/game/Main.java
@@ -6,7 +6,7 @@ import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 public class Main {
     public static void main(String[] args) {
         Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
-        config.setForegroundFPS(0);
+        config.setForegroundFPS(60);
         config.setMaximized(true);
         config.setTitle("pong");
         new Lwjgl3Application(new Game(), config);
