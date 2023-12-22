2b0cadbdc0815f83ddea0d7edec1cfaef5a6e6cb
approachcircle <osc-mcd@protonmail.com>
2023-12-22 21:22:38 +0000

    update ScreenManager to get the current ScreenStack

--- src/main/java/net/approachcircle/game/backend/ScreenManager.java
+++ src/main/java/net/approachcircle/game/backend/ScreenManager.java
@@ -1,9 +1,5 @@
 package net.approachcircle.game.backend;
 
 public interface ScreenManager {
-    Screen getCurrentScreen();
-    void setCurrentScreen(Screen newScreen, boolean clearOnRender);
-    default void setCurrentScreen(Screen newScreen) {
-        setCurrentScreen(newScreen, true);
-    }
+    ScreenStack getScreenStack();
 }
