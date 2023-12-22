e2aa50341796deff7da92eb50c5998e75eb01804
approachcircle <osc-mcd@protonmail.com>
2023-12-22 21:24:03 +0000

    implement new ScreenStack

--- src/main/java/net/approachcircle/game/PongScreen.java
+++ src/main/java/net/approachcircle/game/PongScreen.java
@@ -1,6 +1,9 @@
 package net.approachcircle.game;
 
+import net.approachcircle.game.backend.InputManager;
 import net.approachcircle.game.backend.Screen;
+import net.approachcircle.game.backend.ScreenManager;
+import net.approachcircle.game.backend.ScreenStack;
 
 
 public class PongScreen extends Screen {
@@ -8,8 +11,10 @@ public class PongScreen extends Screen {
     private final Opponent opponent;
     private final Ball ball;
     private final Score score;
+    private final ScreenStack screenStack;
 
     public PongScreen() {
+        this.screenStack = Game.getInstance().getScreenStack();
         player = Player.getInstance();
         opponent = Opponent.getInstance();
         ball = Ball.getInstance();
@@ -22,5 +27,21 @@ public class PongScreen extends Screen {
         opponent.render();
         ball.render();
         score.render();
+        if (score.playerWon()) {
+            screenStack.push(new VictoryScreen());
+        }
+        if (score.opponentWon()) {
+            screenStack.push(new DefeatScreen());
+        }
+        if (gameFinished()) {
+            ball.center();
+            player.centerY();
+            opponent.centerY();
+            score.reset();
+        }
+    }
+
+    private boolean gameFinished() {
+        return score.playerWon() || score.opponentWon();
     }
 }
