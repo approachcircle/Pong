3588e2698941e9b0f374cfcf7204eb3a48236af3
approachcircle <osc-mcd@protonmail.com>
2023-12-22 21:22:08 +0000

    add ScreenStack object

---
+++ src/main/java/net/approachcircle/game/backend/ScreenStack.java
@@ -0,0 +1,30 @@
+package net.approachcircle.game.backend;
+
+import java.util.Stack;
+
+public class ScreenStack {
+    private final Stack<Screen> stack;
+    public ScreenStack() {
+        stack = new Stack<>();
+    }
+
+    public Screen pop() {
+        return stack.pop();
+    }
+
+    public void push(Screen screen) {
+        stack.push(screen);
+    }
+
+    public Screen peek() {
+        return stack.peek();
+    }
+
+    public void insert(int index, Screen screen) {
+        stack.add(index, screen);
+    }
+
+    public boolean isEmpty() {
+        return stack.isEmpty();
+    }
+}
