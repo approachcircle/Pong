2610599af6b190af70487168efd89c5c1ba32823
approachcircle <osc-mcd@protonmail.com>
2023-12-23 23:14:09 +0000

    instantiate new PongScreen instead of .pop() back to last instance

--- src/main/java/net/approachcircle/game/ResultScreen.java
+++ src/main/java/net/approachcircle/game/ResultScreen.java
@@ -12,13 +12,14 @@ public class ResultScreen extends Screen {
     private final int mm_button_padding = 25;
     private final Button retryButton;
     private final Button mainMenuButton;
-    public ResultScreen(Outcome outcome) {
+    public ResultScreen(Outcome outcome, Difficulty lastDifficulty) {
         text = new TextRenderable("You " + outcome.name().toLowerCase() + "!", 1.5f);
         text.centerX();
         text.setY(Gdx.graphics.getHeight() - text_padding);
         retryButton = new Button("Retry", true, (x, y, b) -> {
             Game.getInstance().getScreenStack().pop();
             Game.getInstance().getScreenStack().pop();
+            Game.getInstance().getScreenStack().push(new PongScreen(lastDifficulty));
         }, Game.getInstance());
         retryButton.center();
         retryButton.setY(retryButton.getY() + retry_button_padding);
