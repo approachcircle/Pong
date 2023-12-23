ba7b52696275f3aa0604577083e08b9b6f3016e5
approachcircle <osc-mcd@protonmail.com>
2023-12-23 23:31:17 +0000

    make game run in fullscreen

--- src/main/java/net/approachcircle/game/Main.java
+++ src/main/java/net/approachcircle/game/Main.java
@@ -2,13 +2,15 @@ package net.approachcircle.game;
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Graphics;
 
 public class Main {
     public static void main(String[] args) {
         Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
         config.setForegroundFPS(60);
         config.setMaximized(true);
-        config.setTitle("pong");
+        config.setFullscreenMode(Lwjgl3ApplicationConfiguration.getDisplayMode());
+        config.setTitle("Pong");
         new Lwjgl3Application(Game.getInstance(), config);
     }
 }
