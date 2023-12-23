59f33e2c5522ae25a675e5ad9d58d40e561f483a
approachcircle <osc-mcd@protonmail.com>
2023-12-23 03:29:11 +0000

    add shadow for .jar compilation

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    id("com.github.johnrengelman.shadow").version("8.1.1")
 }
 
 group = "org.example"
@@ -17,6 +18,12 @@ dependencies {
     // implementation("com.badlogicgames.gdx:gdx-platform:1.12.1")
     implementation("com.badlogicgames.gdx:gdx-platform:1.12.1:natives-desktop")
     implementation("com.badlogicgames.gdx-controllers:gdx-controllers-desktop:2.2.3")
+}
+
+tasks.jar {
+    manifest {
+        attributes["Main-Class"] = "net.approachcircle.game.Main"
+    }
 }
 
 tasks.test {
