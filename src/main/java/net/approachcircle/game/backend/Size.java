4a49cc4f63e455ca276f88775ca149c9b4f9a08a
approachcircle <osc-mcd@protonmail.com>
2023-12-23 22:05:30 +0000

    add Size class

---
+++ src/main/java/net/approachcircle/game/backend/Size.java
@@ -0,0 +1,7 @@
+package net.approachcircle.game.backend;
+
+public class Size {
+    public float width;
+    public float height;
+    public Size(float width, float height) {this.width = width; this.height = height;}
+}
