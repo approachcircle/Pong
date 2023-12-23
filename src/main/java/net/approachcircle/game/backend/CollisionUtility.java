8003f44eaf9e2b369539623c8cab08d160bf5664
approachcircle <osc-mcd@protonmail.com>
2023-12-23 22:08:56 +0000

    add overload for Size and Vector2 instead of just Transformables

--- src/main/java/net/approachcircle/game/backend/CollisionUtility.java
+++ src/main/java/net/approachcircle/game/backend/CollisionUtility.java
@@ -1,14 +1,21 @@
 package net.approachcircle.game.backend;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.math.Vector2;
 import net.approachcircle.game.Wall;
 
 public class CollisionUtility {
     public static boolean isCollidingWith(Transformable first, Transformable second) {
-        return (first.getX() + (second.getWidth() / 2) <= second.getX() + second.getWidth() &&
-                first.getX() + first.getWidth() >= second.getX() - (second.getWidth() / 2) &&
+        return (first.getX() <= second.getX() + second.getWidth() &&
+                first.getX() + first.getWidth() >= second.getX() - (second.getWidth() * 1.25f) &&
                 first.getY() + (second.getHeight() / 2) <= second.getY() + second.getHeight() &&
                 first.getY() + first.getHeight() >= second.getY() - (first.getHeight() / 2));
+    }
+    public static boolean isCollidingWith(Vector2 firstPosition, Size firstSize, Vector2 secondPosition, Size secondSize) {
+        return (firstPosition.x <= secondPosition.x + secondSize.width &&
+                firstPosition.x + firstSize.width >= secondPosition.x - (secondSize.width * 1.25f) &&
+                firstPosition.y + (secondSize.height / 2) <= secondPosition.y + secondSize.height &&
+                firstPosition.y + firstSize.height >= secondPosition.y - (firstSize.height / 2));
     }
 
     public static boolean isCollidingWithWall(Transformable object, Wall wall) {
