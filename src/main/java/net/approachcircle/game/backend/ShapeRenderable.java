b5fa30a0047bfdd45c760cf5a6b80dbc138a781d
approachcircle <osc-mcd@protonmail.com>
2023-12-27 00:11:43 +0000

    refactor: remove comment

--- src/main/java/net/approachcircle/game/backend/ShapeRenderable.java
+++ src/main/java/net/approachcircle/game/backend/ShapeRenderable.java
@@ -3,6 +3,5 @@ package net.approachcircle.game.backend;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 
 public abstract class ShapeRenderable implements Renderable {
-    // DISPOSABLE
     protected ShapeRenderer shapeRenderer = new ShapeRenderer();
 }
