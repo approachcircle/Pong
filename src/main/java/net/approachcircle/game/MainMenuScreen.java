30f275070b8efd8de74bccea95d711863dff5db2
approachcircle <osc-mcd@protonmail.com>
2023-12-27 23:29:22 +0000

    add coming soon dialog box to multiplayer button

--- src/main/java/net/approachcircle/game/MainMenuScreen.java
+++ src/main/java/net/approachcircle/game/MainMenuScreen.java
@@ -1,9 +1,7 @@
 package net.approachcircle.game;
 
 import com.badlogic.gdx.Gdx;
-import net.approachcircle.game.backend.Button;
-import net.approachcircle.game.backend.Screen;
-import net.approachcircle.game.backend.TextRenderable;
+import net.approachcircle.game.backend.*;
 
 public class MainMenuScreen extends Screen {
     private final TextRenderable title;
@@ -12,8 +10,10 @@ public class MainMenuScreen extends Screen {
     private final int title_padding = 50;
     private final int sp_button_padding = 50;
     private final int mp_button_padding = 25;
+    private final DialogBox comingSoon;
 
     public MainMenuScreen() {
+        comingSoon = new DialogBox(DialogType.Information, "coming soon!");
         title = new TextRenderable("Pong", 1.5f);
         title.centerX();
         title.setY(Gdx.graphics.getHeight() - title_padding);
@@ -22,7 +22,9 @@ public class MainMenuScreen extends Screen {
         }, Game.getInstance());
         singleplayerButton.center();
         singleplayerButton.setY(singleplayerButton.getY() + sp_button_padding);
-        multiplayerButton = new Button("Multiplayer", true);
+        multiplayerButton = new Button("Multiplayer", true, (x, y, b) -> {
+            comingSoon.toggle();
+        }, Game.getInstance());
         multiplayerButton.center();
         multiplayerButton.setY(multiplayerButton.getY() - (sp_button_padding + mp_button_padding));
     }
@@ -31,5 +33,6 @@ public class MainMenuScreen extends Screen {
         title.render();
         singleplayerButton.render();
         multiplayerButton.render();
+        comingSoon.render();
     }
 }
