65bea7746839a56cdfbbb431164d81f56c1c512a
approachcircle <osc-mcd@protonmail.com>
2024-01-19 14:21:26 +0000

    calculate screen center dynamically, closes #9

--- src/main/java/net/approachcircle/game/backend/ScreenUtility.java
+++ src/main/java/net/approachcircle/game/backend/ScreenUtility.java
@@ -4,18 +4,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.math.Vector2;
 
 public class ScreenUtility {
-    private static Vector2 SCREEN_CENTER;
-    private static boolean initialised = false;
-
-    public static void initialise() {
-        initialised = true;
-        SCREEN_CENTER = new Vector2((float) Gdx.graphics.getWidth() / 2, (float) Gdx.graphics.getHeight() / 2);
-    }
-
     public static Vector2 getScreenCenter() {
-        if (!initialised) {
-            throw new RuntimeException("ScreenUtility not initialised");
-        }
-        return SCREEN_CENTER;
+        return new Vector2((float) Gdx.graphics.getWidth() / 2, (float) Gdx.graphics.getHeight() / 2);
     }
 }
