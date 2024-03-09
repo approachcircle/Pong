ddb41304d3e71797e80eacc6545df2461b539499
approachcircle <osc-mcd@protonmail.com>
2024-03-09 00:51:01 +0000

    add create room error notifications

--- src/main/java/net/approachcircle/game/MultiplayerCreationScreen.java
+++ src/main/java/net/approachcircle/game/MultiplayerCreationScreen.java
@@ -2,10 +2,7 @@ package net.approachcircle.game;
 
 import com.badlogic.gdx.Gdx;
 import net.approachcircle.game.backend.*;
-import net.approachcircle.game.network.ConnectionState;
-import net.approachcircle.game.network.GameEvent;
-import net.approachcircle.game.network.ServerConnection;
-import net.approachcircle.game.network.ServerResponse;
+import net.approachcircle.game.network.*;
 
 public class MultiplayerCreationScreen extends Screen {
     private final int PADDING = 50;
@@ -24,6 +21,11 @@ public class MultiplayerCreationScreen extends Screen {
         addMember(joinButton);
         Button createButton = new Button("Create a game", true, (x, y, b) -> {
             ServerResponse response = ServerConnection.getInstance().emitEventSynchronously(GameEvent.CREATE_GAME);
+            if (response.state == State.Error) {
+                Game.getInstance().getNotificationGroup().add(new ErrorNotification(String.format("failed to create a room: %s", response.message)));
+            } else {
+                Game.getInstance().getNotificationGroup().add(new DialogBox(DialogType.Information, String.format("code is: %s", response.message), Game.getInstance()));
+            }
         }, Game.getInstance());
         createButton.center();
         createButton.setX(createButton.getX() + (createButton.getWidth() / 2) + PADDING);
