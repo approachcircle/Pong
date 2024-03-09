460bd57c690b9d58f2fad620e49c774ebbb939e2
approachcircle <osc-mcd@protonmail.com>
2024-03-09 00:18:03 +0000

    add loading animation

---
+++ src/main/java/net/approachcircle/game/backend/LoadingAnimation.java
@@ -0,0 +1,76 @@
+package net.approachcircle.game.backend;
+
+import com.badlogic.gdx.Files;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class LoadingAnimation extends Renderable implements Transformable {
+    private float width;
+    private float height;
+    private float x;
+    private float y;
+    private Texture texture;
+    private final SpriteBatch batch;
+    private int frame = 0;
+
+    public LoadingAnimation() {
+        batch = new SpriteBatch();
+        setWidth(100);
+        setHeight(100);
+        setX(50);
+        setY(50);
+    }
+
+    @Override
+    public void render() {
+        if (frame >= 60) {
+            frame = 0;
+        }
+        texture = new Texture(Gdx.files.getFileHandle(String.format("loading/loading_%d.png", frame), Files.FileType.Classpath));
+        batch.begin();
+        batch.draw(texture, 50, 50, 50, 50);
+        batch.end();
+        frame++;
+    }
+
+    @Override
+    public float getWidth() {
+        return width;
+    }
+
+    @Override
+    public float getHeight() {
+        return height;
+    }
+
+    @Override
+    public void setWidth(float width) {
+        this.width = width;
+    }
+
+    @Override
+    public void setHeight(float height) {
+        this.height = height;
+    }
+
+    @Override
+    public float getX() {
+        return x;
+    }
+
+    @Override
+    public float getY() {
+        return y;
+    }
+
+    @Override
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    @Override
+    public void setY(float y) {
+        this.y = y;
+    }
+}
